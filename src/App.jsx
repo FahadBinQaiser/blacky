cbb48249259b3cfedbfb4449857b874516975c0b
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-12-02 09:47:03 +0500

    File setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import HeroSection from "./components/HeroSection/HeroSection";
 
 function App() {
-  return <></>;
+  return <HeroSection />;
 }
 
 export default App;
