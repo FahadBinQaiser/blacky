97baca4ed33328a16166c61c56d1e868da90a843
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-12-02 14:39:35 +0500

    Fixed structure

--- src/components/UiComponents/GradientButton/GradientButton.jsx
+++ src/components/UiComponents/GradientButton/GradientButton.jsx
@@ -1,14 +1,9 @@
 import React from "react";
+import "./GradientButton.css";
 
 export default function GradientButton() {
   return (
-    <button
-      className="relative overflow-hidden rounded-full px-6 py-3 text-base font-semibold shadow-xl cursor-pointer text-white"
-      style={{
-        background: "linear-gradient(to bottom, #361893 40%, #afbefd 130%)",
-        backdropFilter: "blur(20px)",
-      }}
-    >
+    <button className="relative overflow-hidden rounded-full px-6 py-3 text-base font-medium shadow-xl cursor-pointer text-white gradient-button">
       Analyze My Demos
     </button>
   );
